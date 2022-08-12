05e8161481e2d878cfcffd752d75a35600560454
Zkeq <admin@icodeq.com>
2022-08-12 19:30:54 +0800

    feat(app.tsx): 🎨 引入 icons CSS 初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import { defineComponent } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+import "./App.scss";
 
 export const App = defineComponent({
   setup() {
     return () => (
-      <>
-        <header>
-          导航
-          <ul>
-            <li>
-              <RouterLink to="/">首页</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/about">关于</RouterLink>
-            </li>
-          </ul>
-        </header>
         <div>
           <RouterView />
         </div>
-        <footer>页脚</footer>
-      </>
     );
   },
 });
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,17 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+import s from './Welcome.module.scss';
+import logo from '../assets/icons/mangosteen.svg';
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    return () => (
-    <div> <RouterView /> </div>
+    return () => (<div class={s.wrapper}>
+    <header>
+      <img src={logo} alt="logo" />
+      <h1>Welcome to Mangosteen</h1>
+    </header>
+    <main> <RouterView /> </main>
+    </div>
     )
   }
 })
