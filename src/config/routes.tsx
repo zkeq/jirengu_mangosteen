3fd61733b2db860f8588c35bc45e253d2669c7ca
Zkeq <admin@icodeq.com>
2022-08-12 18:29:52 +0800

    refactor(main.ts): ♻️ 将 main.ts 分成多个组件
    src/config/routes.tsx
    src/shared/history.tsx

---
+++ src/config/routes.tsx
@@ -0,0 +1,8 @@
+import { Foo } from '../views/Foo';
+import { Bar } from '../views/Bar';
+
+
+export const routes = [
+    { path: '/', component: Foo },
+    { path: '/about', component: Bar },
+  ]
