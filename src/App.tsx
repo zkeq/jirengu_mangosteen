d33c5961971431d687e070dc7be45268365a1eba
Zkeq <admin@icodeq.com>
2022-08-12 16:51:50 +0800

    feat(app.tsx): ✨ 添加导航和路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { defineComponent } from "vue";
-import { RouterView } from "vue-router";
+import { RouterLink, RouterView } from "vue-router";
 
 export const App = defineComponent({
-    setup(){
-        return () =><>
-            <div>
-            <RouterView />
-            </div>
-        </>
-    }
-})
+  setup() {
+    return () => (
+      <>
+        <header>
+          导航
+          <ul>
+            <li>
+              <RouterLink to="/">首页</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/about">关于</RouterLink>
+            </li>
+          </ul>
+        </header>
+        <div>
+          <RouterView />
+        </div>
+        <footer>页脚</footer>
+      </>
+    );
+  },
+});
