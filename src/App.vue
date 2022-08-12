5b13d5e8da32e0786212dd12cf0514eabc38f208
Zkeq <learnonly@learnonly.xyz>
2022-08-12 14:28:10 +0800

    feat(App.vue): ✨ 添加计时器示例代码

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    Hi
+    {{count}}
+  </div>
+  <div>
+    <button @click="onClick">+1</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0)
+const onClick = ()=>{
+  count.value += 1
+}
+</script>
+
+<style>
+</style>
