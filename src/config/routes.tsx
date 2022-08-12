ded2bb28fa896b7a60f8042c28ae43b2e8979298
Zkeq <admin@icodeq.com>
2022-08-12 18:59:41 +0800

    feat(Welcome): ✨ 创建欢迎页面的界面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,8 +1,23 @@
-import { Foo } from '../views/Foo';
-import { Bar } from '../views/Bar';
+import { Foo } from "../views/Foo";
+import { Bar } from "../views/Bar";
+import { RouteRecordRaw } from "vue-router";
+import { First } from "../components/welcome/first";
+import { Second } from '../components/welcome/second';
+import { Third } from "../components/welcome/third";
+import { Forth } from "../components/welcome/forth";
+import { Welcome } from "../views/Welcome";
 
-
-export const routes = [
-    { path: '/', component: Foo },
-    { path: '/about', component: Bar },
-  ]
+export const routes: RouteRecordRaw[] = [
+  { path: "/", component: Foo },
+  { path: "/about", component: Bar },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      { path: "1", component: First },
+      { path: "2", component: Second },
+      { path: "3", component: Third },
+      { path: "4", component: Forth },
+    ],
+  },
+];
