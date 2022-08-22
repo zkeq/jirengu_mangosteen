54f69cc67602fb4277077561b67332097f483eed
Zkeq <admin@icodeq.com>
2022-08-22 11:08:50 +0800

    fix(Welcome.tsx): 🐛 搞定 4 张欢迎卡片的切换

--- src/hooks/useSwipe.tsx
+++ src/hooks/useSwipe.tsx
@@ -5,9 +5,18 @@ type Point = {
   y: number;
 }
 
-export const useSwipe = (element: Ref<HTMLElement | null>) => {
-  const start = ref<Point | null>(null)
-  const end = ref<Point | null>(null)
+interface Options {
+  beforeStart?: (e: TouchEvent) => void
+  afterStart?: (e: TouchEvent) => void
+  beforeMove?: (e: TouchEvent) => void
+  afterMove?: (e: TouchEvent) => void
+  beforeEnd?: (e: TouchEvent) => void
+  afterEnd?: (e: TouchEvent) => void
+}
+
+export const useSwipe = (element: Ref<HTMLElement | undefined>, options?: Options) => {
+  const start = ref<Point>()
+  const end = ref<Point>()
   const swiping = ref(false)
   const distance = computed(() => {
     if (!start.value || !end.value) { return null }
@@ -26,22 +35,28 @@ export const useSwipe = (element: Ref<HTMLElement | null>) => {
     }
   })
   const onStart = (e: TouchEvent) => {
+    options?.beforeStart?.(e)
     swiping.value = true
     end.value = start.value = { x: e.touches[0].screenX, y: e.touches[0].screenY }
+    options?.afterStart?.(e)
   }
   const onMove = (e: TouchEvent) => {
+    options?.beforeMove?.(e)
     if (!start.value) { return }
     end.value = { x: e.touches[0].screenX, y: e.touches[0].screenY, }
+    options?.afterMove?.(e)
   }
   const onEnd = (e: TouchEvent) => {
+    options?.beforeEnd?.(e)
     swiping.value = false
+    options?.afterEnd?.(e)
   }
 
   onMounted(() => {
     if (!element.value) { return }
-    element.value.addEventListener('touchstart', onStart)
-    element.value.addEventListener('touchmove', onMove)
-    element.value.addEventListener('touchend', onEnd)
+    element.value.addEventListener('touchstart', onStart, {passive: false, capture: false})
+    element.value.addEventListener('touchmove', onMove, {passive: false, capture: false})
+    element.value.addEventListener('touchend', onEnd, {passive: false, capture: false})
   })
   onUnmounted(() => {
     if (!element.value) { return }
