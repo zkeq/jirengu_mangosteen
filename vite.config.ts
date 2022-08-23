db4bbee45698b010929f34b2cfd24aa5c5edc8f1
Zkeq <admin@icodeq.com>
2022-08-23 13:02:24 +0800

    feat(src/components/item/InputPad.tsx): ✨ 引入 vant 实现日期选择

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import styleImport, { VantResolve } from 'vite-plugin-style-import';
 // @ts-nocheck
 import { svgstore } from './src/vite_plugins/svgstore';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/', // build path
   plugins: [
     vue(),
     vueJsx({
       transformOn: true,
-      mergeProps: true,
+      mergeProps: true
     }),
     svgstore(),
+    styleImport({
+      resolves: [VantResolve()],
+    }),
   ]
 })
