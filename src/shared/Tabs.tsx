50152c3be55eac174e17d8a5448b0c816d1f9c00
Zkeq <admin@icodeq.com>
2022-08-24 22:30:00 +0800

    feat(src/layouts/TimeTabsLayout.tsx): ✨ tags_id 改为 tag_ids

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -1,5 +1,5 @@
-import { defineComponent, PropType } from 'vue'
-import s from './Tabs.module.scss'
+import { defineComponent, PropType } from 'vue';
+import s from './Tabs.module.scss';
 export const Tabs = defineComponent({
   props: {
     classPrefix: {
@@ -7,7 +7,7 @@ export const Tabs = defineComponent({
     },
     selected: {
       type: String as PropType<string>,
-      required: false
+      required: false,
     },
     rerenderOnSelect: {
       type: Boolean as PropType<boolean>,
@@ -25,32 +25,29 @@ export const Tabs = defineComponent({
         }
       }
       const cp = props.classPrefix
-      return (
-        <div class={[s.tabs, cp + '_tabs']}>
-          <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
-            {tabs.map((item) => (
-              <li
-                class={[
-                  item.props?.name === props.selected ? [s.selected, cp + '_selected'] : '',
-                  cp + '_tabs_nav_item'
-                ]}
-                onClick={() => context.emit('update:selected', item.props?.name)}
-              >
-                {item.props?.name}
-              </li>
-            ))}
-          </ol>
-          {props.rerenderOnSelect ? (
-            <div key={props.selected}>{tabs.find((item) => item.props?.name === props.selected)}</div>
-          ) : (
-            <div>
-              {tabs.map((item) => (
-                <div v-show={item.props?.name === props.selected}>{item}</div>
-              ))}
-            </div>
+      return <div class={[s.tabs, cp + '_tabs']}>
+        <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
+          {tabs.map(item =>
+            <li class={[
+              item.props?.value === props.selected ? [s.selected, cp + '_selected'] : '',
+              cp + '_tabs_nav_item'
+            ]}
+              onClick={() => context.emit('update:selected', item.props?.value)}
+            >
+              {item.props?.name}
+            </li>)}
+        </ol>
+        {props.rerenderOnSelect ?
+          <div key={props.selected}>
+            {tabs.find(item=>item.props?.value === props.selected)}
+          </div>:
+          <div>
+          {tabs.map(item =>
+            <div v-show={item.props?.value === props.selected}>{item}</div>
           )}
-        </div>
-      )
+          </div>
+        }
+      </div>
     }
   }
 })
@@ -58,10 +55,17 @@ export const Tabs = defineComponent({
 export const Tab = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
+      type: String as PropType<string>,
+      required: true
+    },
+    value: {
+      type: String as PropType<string>,
+      required: true
     }
   },
   setup: (props, context) => {
-    return () => <div>{context.slots.default?.()}</div>
+    return () => (
+      <div>{context.slots.default?.()}</div>
+    )
   }
 })
--- src/shared/validate.tsx
+++ src/shared/validate.tsx
@@ -1,10 +1,14 @@
 interface FData {
-  [k: string]: string | number | null | undefined | FData
+  [k: string]: JSONValue
 }
 type Rule<T> = {
   key: keyof T
   message: string
-} & ({ type: 'required' } | { type: 'pattern'; regex: RegExp })
+} & (
+    { type: 'required' } |
+    { type: 'pattern', regex: RegExp } |
+    { type: 'notEqual', value: JSONValue }
+  )
 type Rules<T> = Rule<T>[]
 export type { Rules, Rule, FData }
 export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
@@ -12,7 +16,7 @@ export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
     [k in keyof T]?: string[]
   }
   const errors: Errors = {}
-  rules.map((rule) => {
+  rules.map(rule => {
     const { key, type, message } = rule
     const value = formData[key]
     switch (type) {
@@ -21,13 +25,18 @@ export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
           errors[key] = errors[key] ?? []
           errors[key]?.push(message)
         }
-        break
+        break;
       case 'pattern':
         if (!isEmpty(value) && !rule.regex.test(value!.toString())) {
           errors[key] = errors[key] ?? []
           errors[key]?.push(message)
         }
-        break
+        break;
+      case 'notEqual':
+        if (!isEmpty(value) || value === rule.value) {
+          errors[key] = errors[key] ?? []
+          errors[key]?.push(message)
+        }
       default:
         return
     }
