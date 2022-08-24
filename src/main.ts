a57b656c59f61671aae2d6d42cbd38b9d90926ce
Zkeq <admin@icodeq.com>
2022-08-24 18:25:29 +0800

    feat(src/shared/me.tsx): ✨ 每个路由都做登录检查

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
+import { routes } from './config/routes';
 import { createApp } from 'vue'
 import { App } from './App'
 import { createRouter } from 'vue-router'
-import { routes } from './config/routes';
 import { history } from './shared/history';
 import '@svgstore';
+import { http } from './shared/Http';
+import { fetchMe, mePromise } from './shared/me';
 
 const router = createRouter({ history, routes })
 
+fetchMe()
+
+router.beforeEach(async (to, from) => {
+  if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in')
+    || to.path === '/start') {
+    return true
+  } else {
+    const path = await mePromise!.then(
+      () => true,
+      () => '/sign_in?return_to=' + to.path
+    )
+    return path
+  }
+})
 
 const app = createApp(App)
 app.use(router)
