cfa95c156619e6b7920c0fa03f7a5d80fd0c3708
Zkeq <admin@icodeq.com>
2022-08-24 19:49:33 +0800

    feat(src/components/item/ItemCreate.tsx): ✨ 可以新增记账了

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,11 @@ declare module '*.vue' {
   export default component
 }
 
+declare module '*.scss' {
+  const content: Record<string, any> = {}
+  export default content
+}
+
 type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
 
 type Tag = {
@@ -14,6 +19,14 @@ type Tag = {
   user_id: number,
   name: string,
   sign: string,
+  kind: expenses | income
+}
+type Item = {
+  id: number
+  user_id: number
+  amount: number
+  tags_id: number[]
+  happen_at: string
   kind: expenses | income
 }
 
@@ -24,4 +37,12 @@ type Resources<T = any> = {
     per_page: number,
     count: number
   }
+}
+
+type Resource<T> = {
+  resource: T
+}
+
+type ResourceError = {
+  errors: Record<string, string[]>
 }
