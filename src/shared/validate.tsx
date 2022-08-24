5ab62ba0254a14b0bc0f635bf80007a42e040e6d
Zkeq <admin@icodeq.com>
2022-08-24 20:02:17 +0800

    feat(src/components/tag/TagCreate.tsx): ✨ 创建标签

--- src/shared/validate.tsx
+++ src/shared/validate.tsx
@@ -1,42 +1,45 @@
 interface FData {
-  [k: string]: string | number | null | undefined | FData;
+  [k: string]: string | number | null | undefined | FData
 }
 type Rule<T> = {
-  key: keyof T;
-  message: string;
-} & ({ type: "required" } | { type: "pattern"; regex: RegExp });
-type Rules<T> = Rule<T>[];
-export type { Rules, Rule, FData };
+  key: keyof T
+  message: string
+} & (
+    { type: 'required' } |
+    { type: 'pattern', regex: RegExp }
+  )
+type Rules<T> = Rule<T>[]
+export type { Rules, Rule, FData }
 export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
   type Errors = {
-    [k in keyof T]?: string[];
-  };
-  const errors: Errors = {};
-  rules.map((rule) => {
-    const { key, type, message } = rule;
-    const value = formData[key];
+    [k in keyof T]?: string[]
+  }
+  const errors: Errors = {}
+  rules.map(rule => {
+    const { key, type, message } = rule
+    const value = formData[key]
     switch (type) {
-      case "required":
+      case 'required':
         if (isEmpty(value)) {
-          errors[key] = errors[key] ?? [];
-          errors[key]?.push(message);
+          errors[key] = errors[key] ?? []
+          errors[key]?.push(message)
         }
         break;
-      case "pattern":
+      case 'pattern':
         if (!isEmpty(value) && !rule.regex.test(value!.toString())) {
-          errors[key] = errors[key] ?? [];
-          errors[key]?.push(message);
+          errors[key] = errors[key] ?? []
+          errors[key]?.push(message)
         }
         break;
       default:
-        return;
+        return
     }
-  });
-  return errors;
-};
+  })
+  return errors
+}
 
 function isEmpty(value: null | undefined | string | number | FData) {
-  return value === null || value === undefined || value === "";
+  return value === null || value === undefined || value === ''
 }
 
 export function hasError(errors: Record<string, string[]>) {
@@ -44,7 +47,7 @@ export function hasError(errors: Record<string, string[]>) {
   // .reduce((result, value) => result + value.length, 0) > 0
   let result = false
   for (let key in errors) {
-    if (errors[key].length > 0) {
+    if (errors[key]?.length > 0) {
       result = true
       break
     }
